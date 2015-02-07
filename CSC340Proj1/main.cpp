4afd9230838009070992c0d79dc46790e12dc74f
NateNanda <filmwise101@gmail.com>
2015-02-07 12:48:38 -0800

    Initial Commit

---
+++ CSC340Proj1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSC340Proj1
+//
+//  Created by Nate nanda on 2/7/15.
+//  Copyright (c) 2015 AnuragNanda. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
